4a5abe2aa84e98761a9a1301430631284e6f16e0
rafaldziuryk <rafal.dziuryk@appvinio.com>
2022-10-19 13:13:30 +0200

    add bloc

--- lib/pokemon-list.dart
+++ lib/pokemon-list.dart
@@ -5,9 +5,11 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:weather/pokemon.dart';
+import 'package:weather/pokemon_list_block/pokemon_list_bloc.dart';
 
 class PokemonList extends StatefulWidget {
   const PokemonList({Key? key}) : super(key: key);
@@ -17,134 +19,95 @@ class PokemonList extends StatefulWidget {
 }
 
 class _PokemonListState extends State<PokemonList> {
-  late final Dio dio;
-  late final Box<Pokemon> box;
-  int? selectedIndex;
-  String filteredValue = "";
-  late final Future<Iterable<Pokemon>> pokemonList;
   TextEditingController editingController = TextEditingController();
 
   @override
-  void initState() {
-    dio = GetIt.instance.get();
-    box = GetIt.instance.get();
-
-    pokemonList = dio.get("pokemon?limit=1154&offset=0").then((snapshot) {
-      final data = snapshot.data as Map;
-      final results = data["results"] as List;
-
-      final map = results.asMap().entries.map((entry) => Pokemon.fromMap(
-            index: entry.key,
-            map: entry.value,
-          ));
-
-      print("loaded GET");
-      box.addAll(map);
-      return map;
-    });
-    super.initState();
-
-    print("initState");
-      SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
-        setState(() {
-          // box.values
-          //TODO: wczytanie danych
-          print("Lista z boxa");
-          print(box.values);
-        });
-      });
-    // });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: editingController,
-                  onChanged: (value) {
-                    filteredValue = value;
-                    setState(() {});
-                  },
+    return BlocProvider(
+      create: (context) => PokemonListBloc(dio: GetIt.instance.get(), box: GetIt.instance.get())..add(PokemonListInitEvent()),
+      child: Builder(
+        builder: (context) {
+          return Scaffold(
+            body: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: editingController,
+                        onChanged: (value) {
+                          BlocProvider.of<PokemonListBloc>(context).add(PokemonListSearchEvent(filter: value));
+                        },
+                      ),
+                    ),
+                    FloatingActionButton(
+                      child: Icon(Icons.clear),
+                      onPressed: () {
+                        editingController.clear();
+                        BlocProvider.of<PokemonListBloc>(context).add(PokemonListSearchEvent(filter: editingController.text));
+                      },
+                    ),
+                  ],
                 ),
-              ),
-              FloatingActionButton(
-                child: Icon(Icons.clear),
-                onPressed: () {
-                  filteredValue = "";
-                  editingController.clear();
-                  setState(() {});
-                },
-              ),
-            ],
-          ),
-          Expanded(
-            child: FutureBuilder<Iterable<Pokemon>>(
-                future: box.values.isNotEmpty
-                    ? Future.value(box.values)
-                    : pokemonList,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    final filtered = box.values.where((element) =>
-                        element.name.contains(filteredValue));
-
-                    return ListView.builder(
-                      itemCount: filtered.length,
-                      itemBuilder: (context, index) {
-                        final element = filtered.toList()[index];
-                        return GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              if (selectedIndex == index) {
-                                selectedIndex = null;
-                              } else {
-                                selectedIndex = index;
-                              }
-                            });
-                          },
-                          child: AnimatedContainer(
-                            duration: kThemeAnimationDuration,
-                            height: selectedIndex == index ? 100 : 60,
-                            color: Colors.white,
-                            child: AnimatedOpacity(
+                Expanded(
+                  child: BlocBuilder<PokemonListBloc, PokemonListState> (builder: (context, state) {
+                    if (state is PokemonListInitial)
+                      return Center(child: CircularProgressIndicator());
+                    else if (state is PokemonListPending)
+                      return Center(child: CircularProgressIndicator());
+                    else if (state is PokemonListData) {
+                      return ListView.builder(
+                        itemCount: state.list.length, // filtered.length,
+                        itemBuilder: (context, index) {
+                          final element = state
+                              .list[index]; //  filtered.toList()[index];
+                          return GestureDetector(
+                            onTap: () {
+                              BlocProvider.of<PokemonListBloc>(context).add(PokemonListToggleSelectEvent(selectedIndex: index));
+                            },
+                            child: AnimatedContainer(
                               duration: kThemeAnimationDuration,
-                              opacity:
-                              selectedIndex == index ? 1.0 : 0.8,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  children: [
-                                    Image(
-                                      image: CachedNetworkImageProvider(
-                                        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${element.index + 1}.png",
+                              height: state.selectedIndex == index ? 100 : 60,
+                              color: Colors.white,
+                              child: AnimatedOpacity(
+                                duration: kThemeAnimationDuration,
+                                opacity: state.selectedIndex == index ? 1.0 : 0.5,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children: [
+                                      Image(
+                                        image: CachedNetworkImageProvider(
+                                          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${element
+                                              .index + 1}.png",
+                                        ),
+                                        errorBuilder: (context, error,
+                                            stackTrace) =>
+                                            Text("No image"),
                                       ),
-                                      errorBuilder: (context, error,
-                                          stackTrace) =>
-                                          Text("No image"),
-                                    ),
-                                    Expanded(
-                                        child: Text(
-                                          "Pokemon ${element.name}",
-                                          textAlign: TextAlign.center,
-                                        ))
-                                  ],
+                                      Expanded(
+                                          child: Text(
+                                            "Pokemon ${element.name}",
+                                            textAlign: TextAlign.center,
+                                          ))
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        );
-                      },
-                    );
-                  } else {
-                    return Center(child: CircularProgressIndicator());
-                  }
-                }),
-          ),
-        ],
+                          );
+                        },
+                      );
+                    }
+                    else
+                      return SizedBox.shrink();
+
+                  },),
+                ),
+              ],
+            ),
+          );
+        }
       ),
     );
   }
